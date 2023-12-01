2a503e9cbd6ed2a61bb999b52b1f38866959c23b
Artem <hartem472@gmail.com>
2023-12-01 10:43:17 +0100

    update settings

--- newspaper_agency/settings.py
+++ newspaper_agency/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+SECRET_KEY = os.environ.get('SECRET_KEY', default='1ay!z1%(6or+h^lav^jne*vwxnfo_%=uf$sy((@js28*9w5v6f')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = 'RENDER' not in os.environ
